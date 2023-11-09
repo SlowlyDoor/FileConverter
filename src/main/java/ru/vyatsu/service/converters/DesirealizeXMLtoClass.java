6b4287375f3da831f3c63561c7ea79db3919681d
HELL <internalhell@yandex.ru>
2023-11-09 15:33:45 +0300

    Конвертация из xml в json

--- src/main/java/ru/vyatsu/service/converters/DesirealizeXMLtoClass.java
+++ src/main/java/ru/vyatsu/service/converters/DesirealizeXMLtoClass.java
@@ -1,26 +1,101 @@
 package ru.vyatsu.service.converters;
 
-import ru.vyatsu.service.structure.MangalibXML;
+import ru.vyatsu.service.structure.Mangalib;
+import ru.vyatsu.service.structure.Manhwa;
 
+import javax.json.*;
+import javax.json.stream.JsonGenerator;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
-public class DesirealizeXMLtoClass {
-    public MangalibXML deserialize(String xmlFilePath) {
+public class DesirealizeXMLtoClass
+{
+    public void Converter(String xmlPath, String jsonPath)
+    {
+        Mangalib manhws = deserialize(xmlPath);
+        WriteToJson(jsonPath, manhws);
+    }
+    private Mangalib deserialize(String xmlFilePath) {
         try {
             // Создаем контекст JAXB
-            JAXBContext context = JAXBContext.newInstance(MangalibXML.class);
+            JAXBContext context = JAXBContext.newInstance(Mangalib.class);
 
             // Создаем десериализатор
             Unmarshaller unmarshaller = context.createUnmarshaller();
 
             // Десериализуем XML-файл в объект Mangalib
-            return (MangalibXML) unmarshaller.unmarshal(new File(xmlFilePath));
+            return (Mangalib) unmarshaller.unmarshal(new File(xmlFilePath));
         } catch (JAXBException e) {
             e.printStackTrace();
             return null;
         }
     }
+
+    private void WriteToJson(String jsonPath, Mangalib mangalib) {
+        // Создаем карту для хранения манг по авторам
+        Map<String, JsonArrayBuilder> authorMap = new HashMap<>();
+
+        for (Manhwa manhwa : mangalib.getMangalib()) {
+            // Создаем объекты для манги и ее переводчиков
+            JsonObjectBuilder manhwaBuilder = Json.createObjectBuilder();
+            manhwaBuilder.add("title", manhwa.getTitle());
+            manhwaBuilder.add("publicationYear", manhwa.getPublicationYear());
+            manhwaBuilder.add("chapters", manhwa.getChapters());
+
+            JsonObjectBuilder translationTeamBuilder = Json.createObjectBuilder();
+            translationTeamBuilder.add("publicationProjects", manhwa.getTranslationTeam().getPublicationProjects());
+            translationTeamBuilder.add("adminName", manhwa.getTranslationTeam().getAdminName());
+            translationTeamBuilder.add("name", manhwa.getTranslationTeam().getName());
+
+            manhwaBuilder.add("translationTeam", translationTeamBuilder);
+
+            // Получаем имя автора
+            String authorName = manhwa.getAuthor();
+
+            // Добавляем мангу в соответствующую группу в карте
+            authorMap.computeIfAbsent(authorName, k -> Json.createArrayBuilder())
+                    .add(manhwaBuilder);
+        }
+
+        // Создаем объекты для авторов и манг по авторам
+        JsonArrayBuilder authorsBuilder = Json.createArrayBuilder();
+
+        for (Map.Entry<String, JsonArrayBuilder> entry : authorMap.entrySet()) {
+            JsonObjectBuilder authorBuilder = Json.createObjectBuilder();
+            authorBuilder.add("author", Json.createObjectBuilder()
+                    .add("name", entry.getKey())
+                    .add("manhws", entry.getValue()));
+
+            authorsBuilder.add(authorBuilder);
+        }
+
+        // Строим окончательный JSON объект
+        JsonObject jsonObject = Json.createObjectBuilder()
+                .add("mangalib", Json.createObjectBuilder().add("authors", authorsBuilder))
+                .build();
+
+        // Устанавливаем параметры JsonWriterFactory для красивого вывода
+        Map<String, Object> properties = new HashMap<>(1);
+        // Позволает писать json файл в читабельном виде
+        properties.put(JsonGenerator.PRETTY_PRINTING, true);
+        JsonWriterFactory writerFactory = Json.createWriterFactory(properties);
+
+        // Записываем JSON объект в файл
+        try (OutputStream os = new FileOutputStream(jsonPath)) {
+            JsonWriter jsonWriter = writerFactory.createWriter(os);
+            jsonWriter.writeObject(jsonObject);
+            jsonWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
