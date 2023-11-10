03c9d5948299d974b12ad7578d08f337448da05b
HELL <internalhell@yandex.ru>
2023-11-10 12:56:59 +0300

    Добавлен try catch вместо throws, для работы в jar файле

--- src/main/java/ru/vyatsu/service/converters/ConvertJsonToXml.java
+++ src/main/java/ru/vyatsu/service/converters/ConvertJsonToXml.java
@@ -18,74 +18,84 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConvertJsonToXml {
-    public void Convert(String json, String xml) throws FileNotFoundException, JAXBException {
+    public void Convert(String json, String xml)  {
         Mangalib mangalib = ReadJson(json);
         WriteXml(xml, mangalib);
     }
 
-    private void WriteXml(String path, Mangalib mangalib) throws JAXBException {
-        JAXBContext context = JAXBContext.newInstance(Mangalib.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+    private void WriteXml(String path, Mangalib mangalib) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(Mangalib.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
-        marshaller.marshal(mangalib, new File(path));
+            marshaller.marshal(mangalib, new File(path));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 
-    private Mangalib ReadJson(String path) throws FileNotFoundException {
-        FileInputStream inputStream = new FileInputStream(path);
+    private Mangalib ReadJson(String path) {
+        try {
+            FileInputStream inputStream = new FileInputStream(path);
 
-        JsonParserFactory factory = Json.createParserFactory(null);
-        JsonParser parser = factory.createParser(inputStream, StandardCharsets.UTF_8);
+            JsonParserFactory factory = Json.createParserFactory(null);
+            JsonParser parser = factory.createParser(inputStream, StandardCharsets.UTF_8);
 
-        if (!parser.hasNext() && parser.next() != JsonParser.Event.START_OBJECT) return null;
+            if (!parser.hasNext() && parser.next() != JsonParser.Event.START_OBJECT) return null;
 
-        Mangalib mangalib = new Mangalib();
-        List<Manhwa> manhwaList = new ArrayList<>();
+            Mangalib mangalib = new Mangalib();
+            List<Manhwa> manhwaList = new ArrayList<>();
 
-        while (parser.hasNext()) {
-            JsonParser.Event event = parser.next();
-            if (event == JsonParser.Event.KEY_NAME && parser.getString().equals("authors")) {
-                event = parser.next();
-                if (event == JsonParser.Event.START_ARRAY) {
-                    JsonArray authorsArray = parser.getArray();
-                    for (JsonValue authorValue : authorsArray) {
-                        JsonObject authorObject = authorValue.asJsonObject();
-                        if (authorObject.containsKey("author")) {
-                            JsonObject author = authorObject.getJsonObject("author");
+            while (parser.hasNext()) {
+                JsonParser.Event event = parser.next();
+                if (event == JsonParser.Event.KEY_NAME && parser.getString().equals("authors")) {
+                    event = parser.next();
+                    if (event == JsonParser.Event.START_ARRAY) {
+                        JsonArray authorsArray = parser.getArray();
+                        for (JsonValue authorValue : authorsArray) {
+                            JsonObject authorObject = authorValue.asJsonObject();
+                            if (authorObject.containsKey("author")) {
+                                JsonObject author = authorObject.getJsonObject("author");
 
-                            if (author.containsKey("name") && author.containsKey("manhws")) {
-                                String authorName = author.getString("name");
-                                JsonArray manhwsArray = author.getJsonArray("manhws");
+                                if (author.containsKey("name") && author.containsKey("manhws")) {
+                                    String authorName = author.getString("name");
+                                    JsonArray manhwsArray = author.getJsonArray("manhws");
 
-                                for (int i = 0; i < manhwsArray.size(); i++) {
-                                    JsonObject manhwaObject = manhwsArray.getJsonObject(i);
-                                    Manhwa manhwa = new Manhwa();
+                                    for (int i = 0; i < manhwsArray.size(); i++) {
+                                        JsonObject manhwaObject = manhwsArray.getJsonObject(i);
+                                        Manhwa manhwa = new Manhwa();
 
-                                    manhwa.setId(String.valueOf(manhwaList.size()));
-                                    manhwa.setAuthor(authorName);
-                                    manhwa.setTitle(manhwaObject.getString("title"));
-                                    manhwa.setPublicationYear(manhwaObject.getInt("publicationYear"));
-                                    manhwa.setChapters(manhwaObject.getInt("chapters"));
+                                        manhwa.setId(String.valueOf(manhwaList.size()));
+                                        manhwa.setAuthor(authorName);
+                                        manhwa.setTitle(manhwaObject.getString("title"));
+                                        manhwa.setPublicationYear(manhwaObject.getInt("publicationYear"));
+                                        manhwa.setChapters(manhwaObject.getInt("chapters"));
 
-                                    if (manhwaObject.containsKey("translationTeam")) {
-                                        JsonObject translationTeamObject = manhwaObject.getJsonObject("translationTeam");
-                                        TeamTranslation translationTeam = new TeamTranslation();
-                                        translationTeam.setName(translationTeamObject.getString("name"));
-                                        translationTeam.setPublicationProjects(translationTeamObject.getInt("publicationProjects"));
-                                        translationTeam.setAdminName(translationTeamObject.getString("adminName"));
-                                        manhwa.setTranslationTeam(translationTeam);
+                                        if (manhwaObject.containsKey("translationTeam")) {
+                                            JsonObject translationTeamObject = manhwaObject.getJsonObject("translationTeam");
+                                            TeamTranslation translationTeam = new TeamTranslation();
+                                            translationTeam.setName(translationTeamObject.getString("name"));
+                                            translationTeam.setPublicationProjects(translationTeamObject.getInt("publicationProjects"));
+                                            translationTeam.setAdminName(translationTeamObject.getString("adminName"));
+                                            manhwa.setTranslationTeam(translationTeam);
+                                        }
+
+                                        manhwaList.add(manhwa);
                                     }
-
-                                    manhwaList.add(manhwa);
                                 }
                             }
                         }
                     }
                 }
             }
-        }
 
-        mangalib.setMangalib(manhwaList);
-        return mangalib;
+            mangalib.setMangalib(manhwaList);
+            return mangalib;
+        } catch (FileNotFoundException e) {
+            // Обработка или логирование исключения по необходимости
+            e.printStackTrace();
+            return null;
+        }
     }
 }
