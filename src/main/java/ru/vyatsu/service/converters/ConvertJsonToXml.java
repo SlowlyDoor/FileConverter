60e641701ef75eee52fe390b30db9f591ce25439
SlowlyDoor <internalhell@yandex.ru>
2023-11-24 12:03:17 +0300

    Изменение конверторов под parserFactory

--- src/main/java/ru/vyatsu/service/converters/ConvertJsonToXml.java
+++ src/main/java/ru/vyatsu/service/converters/ConvertJsonToXml.java
@@ -1,105 +1,31 @@
 package ru.vyatsu.service.converters;
 
-import ru.vyatsu.service.structure.Mangalib;
-import ru.vyatsu.service.structure.Manhwa;
-import ru.vyatsu.service.structure.TeamTranslation;
 
-import javax.json.*;
-import javax.json.stream.JsonParser;
-import javax.json.stream.JsonParserFactory;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
+import ru.vyatsu.service.Parser;
+import ru.vyatsu.service.ParserFactory;
+import ru.vyatsu.service.interfaces.Operation;
+import ru.vyatsu.service.structure.json.MangalibJson;
+import ru.vyatsu.service.structure.xml.Mangalib;
 
-public class ConvertJsonToXml {
-    public void Convert(String json, String xml)  {
-        Mangalib mangalib = ReadJson(json);
-        WriteXml(xml, mangalib);
+import java.io.IOException;
+
+public class ConvertJsonToXml implements Operation {
+
+    private Parser<MangalibJson> jsonParser;
+    private Parser<Mangalib> xmlParser;
+    private String inputPath;
+    private String outputPath;
+
+    public ConvertJsonToXml(String inputPath, String outputPath) {
+        this.jsonParser = ParserFactory.createJsonParser();
+        this.xmlParser = ParserFactory.createXmlParser();
+        this.inputPath = inputPath;
+        this.outputPath = outputPath;
     }
 
-    private void WriteXml(String path, Mangalib mangalib) {
-        try {
-            JAXBContext context = JAXBContext.newInstance(Mangalib.class);
-            Marshaller marshaller = context.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-
-            marshaller.marshal(mangalib, new File(path));
-        } catch (JAXBException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Mangalib ReadJson(String path) {
-        try {
-            FileInputStream inputStream = new FileInputStream(path);
-
-            JsonParserFactory factory = Json.createParserFactory(null);
-            JsonParser parser = factory.createParser(inputStream, StandardCharsets.UTF_8);
-
-            if (!parser.hasNext() && parser.next() != JsonParser.Event.START_OBJECT) return null;
-
-            Mangalib mangalib = new Mangalib();
-            List<Manhwa> manhwaList = new ArrayList<>();
-
-            while (parser.hasNext()) {
-                JsonParser.Event event = parser.next();
-                if (event == JsonParser.Event.KEY_NAME && parser.getString().equals("authors")) {
-                    event = parser.next();
-                    if (event == JsonParser.Event.START_ARRAY) {
-                        JsonArray authorsArray = parser.getArray();
-                        for (JsonValue authorValue : authorsArray) {
-                            JsonObject authorObject = authorValue.asJsonObject();
-                            if (authorObject.containsKey("author")) {
-                                JsonObject author = authorObject.getJsonObject("author");
-
-                                if (author.containsKey("name") && author.containsKey("manhws")) {
-                                    String authorName = author.getString("name");
-                                    JsonArray manhwsArray = author.getJsonArray("manhws");
-
-                                    for (int i = 0; i < manhwsArray.size(); i++) {
-                                        JsonObject manhwaObject = manhwsArray.getJsonObject(i);
-                                        Manhwa manhwa = new Manhwa();
-
-                                        manhwa.setId(String.valueOf(manhwaList.size()));
-                                        manhwa.setAuthor(authorName);
-                                        manhwa.setTitle(manhwaObject.getString("title"));
-                                        manhwa.setPublicationYear(manhwaObject.getInt("publicationYear"));
-                                        manhwa.setChapters(manhwaObject.getInt("chapters"));
-
-                                        if (manhwaObject.containsKey("translationTeam")) {
-                                            JsonObject translationTeamObject = manhwaObject.getJsonObject("translationTeam");
-                                            TeamTranslation translationTeam = new TeamTranslation();
-                                            translationTeam.setName(translationTeamObject.getString("name"));
-                                            translationTeam.setPublicationProjects(translationTeamObject.getInt("publicationProjects"));
-                                            translationTeam.setAdminName(translationTeamObject.getString("adminName"));
-                                            manhwa.setTranslationTeam(translationTeam);
-                                        }
-
-                                        manhwaList.add(manhwa);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            mangalib.setMangalib(manhwaList);
-            return mangalib;
-        } catch (FileNotFoundException e) {
-            // Обработка или логирование исключения по необходимости
-            e.printStackTrace();
-            return null;
-        } catch (javax.json.stream.JsonParsingException e) {
-            // Обработка или логирование исключения по необходимости
-            e.printStackTrace();
-            return null;
-        }
+    public void convert() throws IOException {
+        MangalibJson mangalibJson = jsonParser.getReader().read(this.inputPath);
+        Mangalib mangalibXml = MangalibJsonConverter.convertMangalibJsonToMangalib(mangalibJson);
+        xmlParser.getWriter().write(mangalibXml, this.outputPath);
     }
 }
--- src/main/java/ru/vyatsu/service/converters/ConvertXmlToJson.java
+++ src/main/java/ru/vyatsu/service/converters/ConvertXmlToJson.java
@@ -1,122 +1,30 @@
 package ru.vyatsu.service.converters;
 
-import ru.vyatsu.service.structure.Mangalib;
-import ru.vyatsu.service.structure.Manhwa;
 
-import javax.json.*;
-import javax.json.stream.JsonGenerator;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import java.io.File;
-import java.io.FileOutputStream;
+import ru.vyatsu.service.Parser;
+import ru.vyatsu.service.ParserFactory;
+import ru.vyatsu.service.interfaces.Operation;
+import ru.vyatsu.service.structure.json.MangalibJson;
+import ru.vyatsu.service.structure.xml.Mangalib;
+
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
-import java.io.*;
-public class ConvertXmlToJson
-{
-    public void Converter(String xmlPath, String jsonPath)
-    {
-        Mangalib manhws = deserialize(xmlPath);
-        WriteToJson(jsonPath, manhws);
+public class ConvertXmlToJson implements Operation {
+    private Parser<MangalibJson> jsonParser;
+    private Parser<Mangalib> xmlParser;
+    private String inputPath;
+    private String outputPath;
+
+    public ConvertXmlToJson(String inputPath, String outputPath) {
+        this.xmlParser = ParserFactory.createXmlParser();
+        this.jsonParser = ParserFactory.createJsonParser();
+        this.inputPath = inputPath;
+        this.outputPath = outputPath;
     }
-    private Mangalib deserialize(String xmlFilePath) {
-        try {
-            // Создаем контекст JAXB
-            JAXBContext context = JAXBContext.newInstance(Mangalib.class);
-
-            // Создаем десериализатор
-            Unmarshaller unmarshaller = context.createUnmarshaller();
-
-            // Десериализуем XML-файл в объект Mangalib
-            return (Mangalib) unmarshaller.unmarshal(new File(xmlFilePath));
-        } catch (JAXBException e) {
-            e.printStackTrace();
-            return null;
-        }
+    public void convert() throws IOException {
+        Mangalib mangalibXml = xmlParser.getReader().read(this.inputPath);
+        MangalibJson mangalibJson = MangalibConverter.convertMangalibToMangalibJson(mangalibXml);
+        jsonParser.getWriter().write(mangalibJson, this.outputPath);
     }
 
-    private void WriteToJson(String jsonPath, Mangalib mangalib) {
-        // Создаем карту для хранения манг по авторам
-        Map<String, JsonArrayBuilder> authorMap = new LinkedHashMap<>();
-
-        for (Manhwa manhwa : mangalib.getMangalib()) {
-            // Создаем объекты для манги и ее переводчиков
-            JsonObjectBuilder manhwaBuilder = Json.createObjectBuilder();
-            manhwaBuilder.add("title", manhwa.getTitle());
-            manhwaBuilder.add("publicationYear", manhwa.getPublicationYear());
-            manhwaBuilder.add("chapters", manhwa.getChapters());
-
-            JsonObjectBuilder translationTeamBuilder = Json.createObjectBuilder();
-            translationTeamBuilder.add("publicationProjects", manhwa.getTranslationTeam().getPublicationProjects());
-            translationTeamBuilder.add("adminName", manhwa.getTranslationTeam().getAdminName());
-            translationTeamBuilder.add("name", manhwa.getTranslationTeam().getName());
-
-            manhwaBuilder.add("translationTeam", translationTeamBuilder);
-
-            // Получаем имя автора
-            String authorName = manhwa.getAuthor();
-
-            // Добавляем мангу в соответствующую группу в словаре
-            authorMap.computeIfAbsent(authorName, k -> Json.createArrayBuilder())
-                    .add(manhwaBuilder);
-        }
-
-        // Создаем объекты для авторов и манг по авторам
-        JsonArrayBuilder authorsBuilder = Json.createArrayBuilder();
-
-        for (Map.Entry<String, JsonArrayBuilder> entry : authorMap.entrySet()) {
-            JsonObjectBuilder authorBuilder = Json.createObjectBuilder();
-            authorBuilder.add("author", Json.createObjectBuilder()
-                    .add("name", entry.getKey())
-                    .add("manhws", entry.getValue()));
-
-            authorsBuilder.add(authorBuilder);
-        }
-
-        // Строим окончательный JSON объект
-        JsonObject jsonObject = Json.createObjectBuilder()
-                .add("mangalib", Json.createObjectBuilder().add("authors", authorsBuilder))
-                .build();
-
-        // Устанавливаем параметры JsonWriterFactory для читабельного вывода
-        Map<String, Object> properties = new HashMap<>(1);
-        properties.put(JsonGenerator.PRETTY_PRINTING, true);
-        JsonWriterFactory writerFactory = Json.createWriterFactory(properties);
-
-        // Записываем JSON объект в файл
-        try (OutputStream os = new FileOutputStream(jsonPath)) {
-            JsonWriter jsonWriter = writerFactory.createWriter(os);
-            jsonWriter.writeObject(jsonObject);
-            // Удаляем начальную строку в файле
-            removeFirstLine(jsonPath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Метод для удаления первой строки в файле
-    private static void removeFirstLine(String filePath) throws IOException {
-        RandomAccessFile file = new RandomAccessFile(filePath, "rw");
-        // Получаем длину файла
-        long length = file.length();
-        // Устанавливаем указатель на начало файла
-        file.getChannel().position(0);
-        // Создаем буфер для чтения
-        byte[] buffer = new byte[(int) length];
-        file.readFully(buffer);
-        // Записываем остальные данные в файл, исключая первую строку
-        file.getChannel().position(0);
-        file.write(buffer, "\n".length(), buffer.length - "\n".length());
-        // Усекаем файл до новой длины
-        file.getChannel().truncate(length - "\n".length());
-        file.close();
-    }
 }
