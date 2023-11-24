9960c14f6fdba6386fd034689bc8d0e33927c264
SlowlyDoor <internalhell@yandex.ru>
2023-11-24 11:54:00 +0300

    Добавление внутреннего класса для json файла

---
+++ src/main/java/ru/vyatsu/service/structure/json/TeamTranslationJson.java
@@ -0,0 +1,18 @@
+package ru.vyatsu.service.structure.json;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TeamTranslationJson {
+    private Integer publicationProjects;
+    private String adminName;
+    private String name;
+}
