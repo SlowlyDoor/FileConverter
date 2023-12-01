2aea33143d6014f2b1f5a0aba5dfdc0d8cd01bf7
SlowlyDoor <internalhell@yandex.ru>
2023-12-01 14:26:10 +0300

    Изменение логики, в main только запуск и вывод

--- src/main/java/ru/vyatsu/fileconverter/Main.java
+++ src/main/java/ru/vyatsu/fileconverter/Main.java
@@ -1,34 +1,38 @@
 package ru.vyatsu.fileconverter;
 
+import org.apache.log4j.PropertyConfigurator;
 import ru.vyatsu.fileconverter.core.transform.ConverterFactory;
 import ru.vyatsu.fileconverter.core.specification.OperationType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Main {
-    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         try {
-            ConverterFactory.choice(
-                            OperationType.getOperationType(args[0], args[1]),
-                            args[0],
-                            args[1])
-                    .convert();
-
-            System.out.println("Преобразование успешно выполнено!");
-
-        } catch (ArrayIndexOutOfBoundsException e) {
-            String errorMessage = "Необходимо указать два аргумента!";
-            LOGGER.log(Level.SEVERE, errorMessage, e);
-            e.printStackTrace();
-            throw new IllegalArgumentException(errorMessage, e);
-        } catch (Exception e) {
-            String errorMessage = "Произошла ошибка: " + e.getMessage();
-            LOGGER.log(Level.SEVERE, errorMessage, e);
-            e.printStackTrace();
-            throw new RuntimeException(errorMessage, e);
+            processConversion(args);
+            LOGGER.info("Преобразование успешно выполнено!");
+        } catch (ArrayIndexOutOfBoundsException indexOutException) {
+            LOGGER.error("Необходимо указать два аргумента!", indexOutException);
+        } catch (Exception exception) {
+            LOGGER.error("Произошла ошибка: " + exception.getMessage(), exception);
         }
+    }
+
+    private static void processConversion(String[] args) throws IOException {
+        PropertyConfigurator.configure(
+                Objects.requireNonNull(
+                        Main.class.getClassLoader().getResource("log4j.properties")
+                ).getPath()
+        );
+        ConverterFactory.choice(
+                OperationType.getOperationType(args[0], args[1]),
+                args[0],
+                args[1]
+        ).convert();
     }
 }
