a137509958cddcfc21ee1d3b3a1b17a673a6fbe8
SlowlyDoor <internalhell@yandex.ru>
2023-12-01 12:49:33 +0300

    Добавление lombook.Data

--- src/main/java/ru/vyatsu/fileconverter/core/Parser.java
+++ src/main/java/ru/vyatsu/fileconverter/core/Parser.java
@@ -1,26 +1,11 @@
 package ru.vyatsu.fileconverter.core;
 
+import lombok.Data;
 import ru.vyatsu.fileconverter.core.specification.Reader;
 import ru.vyatsu.fileconverter.core.specification.Writer;
 
-
+@Data
 public final class Parser<T> {
     private Writer<T> writer;
     private Reader<T> reader;
-
-    public Writer<T> getWriter() {
-        return writer;
-    }
-
-    public Reader<T> getReader() {
-        return reader;
-    }
-
-    public void setWriter(Writer<T> writer) {
-        this.writer = writer;
-    }
-
-    public void setReader(Reader<T> reader) {
-        this.reader = reader;
-    }
 }
