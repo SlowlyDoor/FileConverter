63e2c1ff7d16211bfbbcb5cd6a2e88f32a4dcdef
SlowlyDoor <internalhell@yandex.ru>
2023-12-01 14:01:59 +0300

    Удаление ветки default

--- src/main/java/ru/vyatsu/fileconverter/core/transform/ConverterFactory.java
+++ src/main/java/ru/vyatsu/fileconverter/core/transform/ConverterFactory.java
@@ -4,11 +4,11 @@ import ru.vyatsu.fileconverter.core.specification.OperationType;
 
 
 public class ConverterFactory {
+    private ConverterFactory(){}
     public static DataConverter choice(OperationType operationType, String inputPath, String outputPath) {
         return switch (operationType) {
             case JSON_TO_XML -> new JsonToXml(inputPath, outputPath);
             case XML_TO_JSON -> new XmlToJson(inputPath, outputPath);
-            default -> throw new IllegalArgumentException("Неподдерживаемый тип операции");
         };
     }
 }
