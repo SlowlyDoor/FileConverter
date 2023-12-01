f273fdbc04e68f8cbd3bfea7c630bf31f9c24017
SlowlyDoor <internalhell@yandex.ru>
2023-12-01 15:22:01 +0300

    Удаление лишних пробелов

--- src/main/java/ru/vyatsu/fileconverter/core/transform/ConverterFactory.java
+++ src/main/java/ru/vyatsu/fileconverter/core/transform/ConverterFactory.java
@@ -2,9 +2,9 @@ package ru.vyatsu.fileconverter.core.transform;
 
 import ru.vyatsu.fileconverter.core.specification.OperationType;
 
-
 public class ConverterFactory {
-    private ConverterFactory(){}
+    private ConverterFactory() {}
+
     public static DataConverter choice(OperationType operationType, String inputPath, String outputPath) {
         return switch (operationType) {
             case JSON_TO_XML -> new JsonToXml(inputPath, outputPath);
