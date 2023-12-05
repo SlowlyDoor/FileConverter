608fe3d7c25b698f64702f665a7a61749c0ca0e6
SlowlyDoor <internalhell@yandex.ru>
2023-12-05 04:32:26 +0300

    Добавление аннотации и форматирование кода

--- src/main/java/ru/vyatsu/fileconverter/Main.java
+++ src/main/java/ru/vyatsu/fileconverter/Main.java
@@ -1,38 +1,32 @@
 package ru.vyatsu.fileconverter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.log4j.PropertyConfigurator;
 import ru.vyatsu.fileconverter.core.ApplicationException;
 import ru.vyatsu.fileconverter.core.transform.ConverterFactory;
 import ru.vyatsu.fileconverter.core.specification.OperationType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
+@Slf4j
 public class Main {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         try {
             processConversion(args);
-            LOGGER.info("Преобразование успешно выполнено!");
+            log.info("Преобразование успешно выполнено!");
         } catch (ArrayIndexOutOfBoundsException indexOutException) {
-            LOGGER.error("Необходимо указать два аргумента!", indexOutException);
+            log.error("Необходимо указать два аргумента!", indexOutException);
         } catch (Exception exception) {
-            LOGGER.error("Произошла ошибка: " + exception.getMessage(), exception);
+            log.error("Произошла ошибка: " + exception.getMessage(), exception);
         }
     }
 
     private static void processConversion(String[] args) throws ApplicationException {
-        PropertyConfigurator.configure(
-                Objects.requireNonNull(
-                        Main.class.getClassLoader().getResource("log4j.properties")
-                ).getPath()
-        );
-        ConverterFactory.choice(
-                OperationType.getOperationType(args[0], args[1]),
-                args[0],
-                args[1]
-        ).convert();
+        PropertyConfigurator.configure(Objects
+                .requireNonNull(Main.class.getClassLoader()
+                        .getResource("log4j.properties")).getPath());
+        ConverterFactory.choice(OperationType.getOperationType(args[0], args[1]),
+                                args[0], args[1]).convert();
     }
 }
